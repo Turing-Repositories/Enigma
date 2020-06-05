c64ffac742aed9bf3b08b7f20524dbd077eb9f0b
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-05 16:57:15 -0600

    :white_check_mark: Outline basic tests for enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,4 +8,43 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_can_encrypt_message_with_key_and_date
+    enigma.encrypt("hello world", "02715", "040895")
+
+    expected = {
+     encryption: "keder ohulw",
+     key: "02715",
+     date: "040895"
+    }
+
+    assert_equal expected, enigma.encrypt
+  end
+
+  def test_it_can_decrypt_message_with_key_and_date
+    enigma.decrypt("keder ohulw", "02715", "040895")
+
+    expected = {
+     decryption: "hello world",
+     key: "02715",
+     date: "040895"
+    }
+
+    assert_equal expected, enigma.decrypt
+  end
+
+  def test_it_can_encrpy_message_with_key_and_todays_date
+    skip
+    encrypted = enigma.encrypt("hello world", "02715")
+  end
+
+  def test_it_can_decrypt_message_with_key_and_todays_date
+    skip
+    enigma.decrypt(encrypted[:encryption], "02715")
+  end
+
+  def test_it_can_encrypt_message_with_random_key_and_todays_date
+    skip
+    enigma.encrypt("hello world")
+  end
 end
