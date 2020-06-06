ecf71f6a09a601ee4c48d818a450da18207f0783
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-06 10:31:22 -0600

    :heavy_check_mark: Pass attributes test and prep for generate random key numbers

--- test/key_test.rb
+++ test/key_test.rb
@@ -10,6 +10,13 @@ class KeyTest < Minitest::Test
   def test_it_has_attributes
     key = Key.new('12345')
 
-    assert_equal '12345', key.key_numbers
+    assert_equal '12345', key.key_set_numbers
+  end
+
+  def test_it_can_generate_random_key_numbers
+     #look up mocks and stubs
+    key = Key.new()
+
+    puts key.key_set_numbers
   end
 end
