d6578e96c58b82bc047e64db634f3b9f0fa82d68
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-07 16:26:53 -0600

    :lipstick: Sort by code length

--- lib/reuseables.rb
+++ lib/reuseables.rb
@@ -1,6 +1,21 @@
 require 'date'
 module Reuseables
 
+  # Encrypt.rb && Decrypt.rb
+  def shift_alphabet_letter(index, amount_of_shift)
+    return alphabet.rotate(amount_of_shift[:A]) if (index % 4).zero?
+    return alphabet.rotate(amount_of_shift[:B]) if index % 4 == 1
+    return alphabet.rotate(amount_of_shift[:C]) if index % 4 == 2
+    return alphabet.rotate(amount_of_shift[:D]) if index % 4 == 3
+  end
+
+  def format_message(message)
+    message.downcase
+  end
+
+  def alphabet
+    ('a'..'z').to_a << ' '
+  end
 
   # Key.rb
   def random_key_numbers
@@ -19,22 +34,6 @@ module Reuseables
 
   def offset_numbers
     date_squared
-  end
-
-  # Encrypt.rb && Decrypt.rb
-  def shift_alphabet_letter(index, amount_of_shift)
-    return alphabet.rotate(amount_of_shift[:A]) if (index % 4).zero?
-    return alphabet.rotate(amount_of_shift[:B]) if index % 4 == 1
-    return alphabet.rotate(amount_of_shift[:C]) if index % 4 == 2
-    return alphabet.rotate(amount_of_shift[:D]) if index % 4 == 3
-  end
-
-  def format_message(message)
-    message.downcase
-  end
-
-  def alphabet
-    ('a'..'z').to_a << ' '
   end
 
   # Date Info
