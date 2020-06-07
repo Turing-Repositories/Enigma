477773b56e0c11e4e53c5a08ffc3c381e0e3a769
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-07 15:35:14 -0600

    :heavy_check_mark: Pass it can decrypt message

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -10,4 +10,27 @@ class Decrypt
     @key = key
     @offset = offset
   end
+
+  def alphabet
+    ('a'..'z').to_a << ' '
+  end
+
+  def shift_alphabet_letter(index, amount_of_shift) #working, but not really sure why?
+    return alphabet.rotate(amount_of_shift[:A]) if index % 4 == 0
+    return alphabet.rotate(amount_of_shift[:B]) if index % 4 == 1
+    return alphabet.rotate(amount_of_shift[:C]) if index % 4 == 2
+    return alphabet.rotate(amount_of_shift[:D]) if index % 4 == 3
+  end
+
+  def decrypt_message(message, amount_of_shift)
+    decrypted_message = ''
+    message.each_char.with_index do |char, index|
+      if alphabet.include?(char)
+      decrypted_message += shift_alphabet_letter(index, amount_of_shift)[alphabet.index(char)]
+      else
+      decrypted_message += char
+      end
+    end
+    decrypted_message
+  end
 end
