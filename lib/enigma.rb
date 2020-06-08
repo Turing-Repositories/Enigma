0727d70e890e8d1eae8bd2744cc74da099976ea8
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-08 17:29:29 -0600

    :heavy_check_mark: Pass encrypt message with key and date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,27 +8,40 @@ class Enigma
   include Reuseables
   attr_reader :message
 
-  def initialize(message, key = random_key_numbers, date)
+  def initialize(message, key, date = todays_date)
     @message = message
     @key = key
     @date = date
     @offset = offset
+
   end
 
-  def encrypt(message, key = random_key_numbers, date)
-    x = Key.new(key)
-    y = Offset.new(date)
-    require "pry"; binding.pry
-    encrypter = Encrypt.new(message, key, date)
-
-    #encrypter.find_encrpytion_keys(key = @key, offset = @offset)
-    #encrypter.encrpytion_keys(numbers = find_encrpytion_keys)
-    #encypter.encrypt_message(message, amount_of_shift)
-    encrypter.final_encryption
+  def encrypt(message, key, date = todays_date)
+    new_key = Key.new(key)
+    a = new_key.generate_key_set_numbers
+    new_offset = Offset.new(date)
+    b = new_offset.generate_offset_numbers
+    c = encrypter = Encrypt.new(message, key, date)
+    d = encrypter.find_encrpytion_keys(a, b)
+    e = encrypter.encrpytion_keys(d)
+    f = encrypter.encrypt_message(message, e)
+    g = encrypter.final_encryption
   end
 
-  def decrypt(message, key = offset_numbers, date = todays_date)
-    decrypter = Decrypt.new(message, key, date)
-    decrypter.final_decryption
+  def decrypt(message, key, date = todays_date)
+    new_key = Key.new(key)
+    a = new_key.generate_key_set_numbers
+    new_offset = Offset.new(date)
+    b = new_offset.generate_offset_numbers
+    c = encrypter = Encrypt.new(message, key, date)
+    d = encrypter.find_encrpytion_keys(a, b)
+    e = encrypter.encrpytion_keys(d)
+
+
+    f = decrypter = Decrypt.new(message, key, date)
+    g = decrypter.find_decrpytion_keys(a, b)
+    h = decrypter.decrpytion_keys(g)
+    i = decrypter.decrypt_message(message, h)
+    j = decrypter.final_decryption
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,8 @@
 require './test/test_helper.rb'
 
 class EnigmaTest < Minitest::Test
+  include Reuseables
+
   def test_it_exists
     enigma = Enigma.new('hello world', '02715', '040895')
 
@@ -17,33 +19,5 @@ class EnigmaTest < Minitest::Test
     }
 
     assert_equal expected, enigma.encrypt('hello world', '02715', '040895')
-  end
-
-  def test_it_can_decrypt_message_with_key_and_date
-    skip
-    enigma.decrypt("keder ohulw", "02715", "040895")
-
-    expected = {
-     decryption: "hello world",
-     key: "02715",
-     date: "040895"
-    }
-
-    assert_equal expected, enigma.decrypt
-  end
-
-  def test_it_can_encrpy_message_with_key_and_todays_date
-    skip
-    encrypted = enigma.encrypt("hello world", "02715")
-  end
-
-  def test_it_can_decrypt_message_with_key_and_todays_date
-    skip
-    enigma.decrypt(encrypted[:encryption], "02715")
-  end
-
-  def test_it_can_encrypt_message_with_random_key_and_todays_date
-    skip
-    enigma.encrypt("hello world")
   end
 end
