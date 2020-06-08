a9c93390c5eb07dbec295199028948e558666d84
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-08 17:23:57 -0600

    :construction: Added default params for date to include todays date

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,11 +4,30 @@ class Decrypt
   include Reuseables
   attr_reader :message
 
-  def initialize(message, key, offset)
+  def initialize(message, key, date = todays_date)
     @message = message
     @key = key
+    @date = date
     @offset = offset
     @decryption_result = ''
+  end
+
+  def find_decrpytion_keys(key = @key, offset = @offset)
+    key_acc = []
+    key.each_value { |v| key_acc << v.to_i}
+    offset_acc = []
+    offset.each_value { |v| offset_acc << v.to_i }
+    keys = key_acc.zip(offset_acc).map { |x, y| (x + y) * -1 }
+    keys
+  end
+
+  def decrpytion_keys(numbers = find_decrpytion_keys)
+    keys = Hash.new
+    keys[:A] = numbers[0]
+    keys[:B] = numbers[1]
+    keys[:C] = numbers[2]
+    keys[:D] = numbers[3]
+    keys
   end
 
   def decrypt_message(message, amount_of_shift)
@@ -26,7 +45,7 @@ class Decrypt
   def final_decryption
     { decryption: @decryption_result,
       key: @key,
-      date: todays_date
+      date: @date
     }
   end
 end
