6aabdbeb510f39dce9d31de292f45302d88f1d1b
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-09 07:05:39 -0600

    :construction: Update decrypt text according to updates made in encrypt.rb

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,51 +1,14 @@
-require_relative 'reuseables'
+require_relative 'enigma'
 
-class Decrypt
-  include Reuseables
-  attr_reader :message
+reader = File.open('lib/'+ARGV[0], "r")
+message = reader.read
+reader.close
 
-  def initialize(message, key, date = todays_date)
-    @message = message
-    @key = key
-    @date = date
-    @offset = offset
-    @decryption_result = ''
-  end
+enigma = Enigma.new(message)
+decrypted = enigma.decrypt(message, enigma.random_key_numbers, enigma.todays_date)
 
-  def find_decrpytion_keys(key = @key, offset = @offset)
-    key_acc = []
-    key.each_value { |v| key_acc << v.to_i}
-    offset_acc = []
-    offset.each_value { |v| offset_acc << v.to_i }
-    keys = key_acc.zip(offset_acc).map { |x, y| (x + y) * -1 }
-    keys
-  end
+decryption = File.open(ARGV[1], "w")
+decryption.write(message, ARGV[2], ARGV[3])
+decryption.close
 
-  def decrpytion_keys(numbers = find_decrpytion_keys)
-    keys = Hash.new
-    keys[:A] = numbers[0]
-    keys[:B] = numbers[1]
-    keys[:C] = numbers[2]
-    keys[:D] = numbers[3]
-    keys
-  end
-
-  def decrypt_message(message, amount_of_shift)
-    decrypted_message = ''
-    message.each_char.with_index do |char, index|
-      if alphabet.include?(char)
-      decrypted_message += shift_alphabet_letter(index, amount_of_shift)[alphabet.index(char)]
-      else
-      decrypted_message += char
-      end
-    end
-    @decryption_result = decrypted_message
-  end
-
-  def final_decryption
-    { decryption: @decryption_result,
-      key: @key,
-      date: @date
-    }
-  end
-end
+puts "Created '#{ARGV[1]}' with the key #{decrypted[:key]} and date #{decrypted[:date]}"
