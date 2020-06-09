20fe01ca9ef9bb8a427ca86e8993d577dc82046a
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-09 08:10:32 -0600

    :zap: Commit to prep merge for eval

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,14 +1,20 @@
 require_relative 'enigma'
 
 reader = File.open('lib/'+ARGV[0], "r")
-message = reader.read
+message = reader.read.strip
 reader.close
 
-enigma = Enigma.new(message)
-decrypted = enigma.decrypt(message, enigma.random_key_numbers, enigma.todays_date)
+puts message
 
-decryption = File.open(ARGV[1], "w")
-decryption.write(message, ARGV[2], ARGV[3])
+enigma = Enigma.new("cggtonzor loscybzwn!", ARGV[2], ARGV[3])
+
+ puts message
+
+decrypted = enigma.decrypt("cggtonzor loscybzwn!", ARGV[2], ARGV[3])
+
+
+decryption = File.open('lib/'+ARGV[1], "w")
+decryption.write(enigma.message, ARGV[2], ARGV[3])
 decryption.close
 
 puts "Created '#{ARGV[1]}' with the key #{decrypted[:key]} and date #{decrypted[:date]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,13 +1,15 @@
 require_relative 'enigma'
 
 reader = File.open('lib/'+ARGV[0], "r")
-message = reader.read
+message = reader.read.strip
 reader.close
+
+  puts message
 
 enigma = Enigma.new(message)
 encrypted = enigma.encrypt(message, enigma.random_key_numbers, enigma.todays_date)
 
-encryption = File.open(ARGV[1], "w")
+encryption = File.open('lib/'+ARGV[1], "w")
 encryption.write(encrypted)
 encryption.close
 
