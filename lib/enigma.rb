55ddc949e6b4086e19617ad4a48441c6d671ce90
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-08 18:22:12 -0600

    :lipstick: Fix method names in enigma to match its method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -18,30 +18,27 @@ class Enigma
 
   def encrypt(message, key, date = todays_date)
     new_key = Key.new(key)
-    a = new_key.generate_key_set_numbers
+    new_key_set = new_key.generate_key_set_numbers
     new_offset = Offset.new(date)
-    b = new_offset.generate_offset_numbers
-    c = encrypter = Encrypt.new(message, key, date)
-    d = encrypter.find_encrpytion_keys(a, b)
-    e = encrypter.encrpytion_keys(d)
-    f = encrypter.encrypt_message(message, e)
-    g = encrypter.final_encryption
+    new_offset_number_set = new_offset.generate_offset_numbers
+    encrypter = Encrypt.new(message, key, date)
+    new_encryption_keys_array = encrypter.find_encrpytion_keys(new_key_set, new_offset_number_set)
+    new_encryption_keys_hash = encrypter.encrpytion_keys(new_encryption_keys_array)
+    encrypter.encrypt_message(message, new_encryption_keys_hash)
+    encrypter.final_encryption
   end
 
   def decrypt(message, key, date = todays_date)
     new_key = Key.new(key)
-    a = new_key.generate_key_set_numbers
+    new_key_set = new_key.generate_key_set_numbers
     new_offset = Offset.new(date)
-    b = new_offset.generate_offset_numbers
-    c = encrypter = Encrypt.new(message, key, date)
-    d = encrypter.find_encrpytion_keys(a, b)
-    e = encrypter.encrpytion_keys(d)
+    new_offset_number_set = new_offset.generate_offset_numbers
+    Encrypt.new(message, key, date)
 
-
-    f = decrypter = Decrypt.new(message, key, date)
-    g = decrypter.find_decrpytion_keys(a, b)
-    h = decrypter.decrpytion_keys(g)
-    i = decrypter.decrypt_message(message, h)
-    j = decrypter.final_decryption
+    decrypter = Decrypt.new(message, key, date)
+    new_decryption_keys_array = decrypter.find_decrpytion_keys(new_key_set, new_offset_number_set)
+    new_decryption_keys_hash = decrypter.decrpytion_keys(new_decryption_keys_array)
+    decrypter.decrypt_message(message, new_decryption_keys_hash)
+    decrypter.final_decryption
   end
 end
