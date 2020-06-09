d3150c048ff94a708d3e6e94d9264dcd9d6c58fc
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-09 07:03:04 -0600

    :bug: Fix bug that was not allowing me to find the right path

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,52 +1,14 @@
-require_relative 'reuseables'
+require_relative 'enigma'
 
-class Encrypt
-  include Reuseables
-  attr_reader :message
+reader = File.open('lib/'+ARGV[0], "r")
+message = reader.read
+reader.close
 
-  def initialize(message, key, date = todays_date)
-    @message = message
-    @key = key
-    @date = date
-    @offset = offset
-    @result = ''
-  end
+enigma = Enigma.new(message)
+encrypted = enigma.encrypt(message, enigma.random_key_numbers, enigma.todays_date)
 
-  def find_encrpytion_keys(key = @key, offset = @offset)
-    key_acc = []
-    key.each_value { |v| key_acc << v.to_i }
-    offset_acc = []
-    offset.each_value { |v| offset_acc << v.to_i }
-    keys = key_acc.zip(offset_acc).map { |x, y| x + y }
-    keys
-  end
+encryption = File.open(ARGV[1], "w")
+encryption.write(encrypted)
+encryption.close
 
-  def encrpytion_keys(numbers = find_encrpytion_keys)
-    keys = Hash.new
-    keys[:A] = numbers[0]
-    keys[:B] = numbers[1]
-    keys[:C] = numbers[2]
-    keys[:D] = numbers[3]
-    keys
-  end
-
-  def encrypt_message(message, amount_of_shift)
-    encrypted_message = ''
-    format_message(message).each_char.with_index do |char, index|
-      if alphabet.include?(char)
-        encrypted_message += shift_alphabet_letter(index, amount_of_shift)[alphabet.index(char)]
-      else
-        encrypted_message += char
-      end
-    end
-    @result = encrypted_message
-  end
-
-  def final_encryption
-    { encryption: @result,
-      key: @key,
-      date: @date
-    }
-  end
-
-end
+puts "Created '#{ARGV[1]}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
